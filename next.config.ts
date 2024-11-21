ae52bb17e9049716a2b8927fc086a8e90271da71
yuxian <1606656170@qq.com>
2024-11-21 14:27:33 +0800

    initial

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // 本地端口设置为 3008
+  serverRuntimeConfig: {
+    port: 3008,
+  },
 };
 
 export default nextConfig;
